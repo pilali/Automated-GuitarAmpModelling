676b0a523aaa9b75f830863e411cb5ebfdca9b9b
Massimo Pennazio <maxipenna@libero.it>
2021-09-10 18:32:51 +0200

    Using split function provided in CoreAudioML.dataset

--- prep_wav.py
+++ prep_wav.py
@@ -1,44 +1,42 @@
+from CoreAudioML.dataset import audio_converter, audio_splitter
 from scipy.io import wavfile
 import argparse
-import numpy as np
-from CoreAudioML.dataset import audio_converter, audio_splitter
 
-def save_wav(name, data):
-    wavfile.write(name, 44100, data.flatten().astype(np.float32))
-
-def normalize(data):
-    data_max = max(data)
-    data_min = min(data)
-    data_norm = max(data_max,abs(data_min))
-    return data / data_norm
+def save_wav(name, rate, data):
+    print("Writing %s with rate=%d" % (name, rate))
+    wavfile.write(name, rate, data.flatten().astype(np.float32))
 
 def main(args):
     # Load and Preprocess Data ###########################################
+    print("Input file name: %s" % args.in_file)
     in_rate, in_data = wavfile.read(args.in_file)
+    print("Output file name: %s" % args.out_file)
     out_rate, out_data = wavfile.read(args.out_file)
+
+    if in_rate != out_rate:
+        print("Error! Sample rate needs to be equal")
+        exit(1)
+    else:
+        rate = in_rate
 
     print("Input rate: %d" % in_rate)
     print("Output rate: %d" % out_rate)
 
     x_all = audio_converter(in_data[1])
-    print("Applying normalization to input")
-    x_all = normalize(x_all).reshape(len(x_all),1)
     y_all = audio_converter(out_data[1])
-    print("Applying normalization to output")
-    y_all = normalize(y_all).reshape(len(y_all),1)
 
     print("Splitting audio as following: 0.70 train 0.15 test 0.15 val")
     splitted_x = audio_splitter(x_all, [0.70, 0.15, 0.15])
     splitted_y = audio_splitter(y_all, [0.70, 0.15, 0.15])
 
-    save_wav(args.path + "/train/" + args.name + "-input.wav", splitted_x[0])
-    save_wav(args.path + "/train/" + args.name + "-target.wav", splitted_y[0])
+    save_wav(args.path + "/train/" + args.name + "-input.wav", rate, splitted_x[0])
+    save_wav(args.path + "/train/" + args.name + "-target.wav", rate, splitted_y[0])
 
-    save_wav(args.path + "/test/" + args.name + "-input.wav", splitted_x[1])
-    save_wav(args.path + "/test/" + args.name + "-target.wav", splitted_y[1])
+    save_wav(args.path + "/test/" + args.name + "-input.wav", rate, splitted_x[1])
+    save_wav(args.path + "/test/" + args.name + "-target.wav", rate, splitted_y[1])
 
-    save_wav(args.path + "/val/" + args.name + "-input.wav", splitted_x[2])
-    save_wav(args.path + "/val/" + args.name + "-target.wav", splitted_y[2])
+    save_wav(args.path + "/val/" + args.name + "-input.wav", rate, splitted_x[2])
+    save_wav(args.path + "/val/" + args.name + "-target.wav", rate, splitted_y[2])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
