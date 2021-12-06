4287e9a1f357aba019a5e3be930013a780976087
Massimo Pennazio <maxipenna@libero.it>
2021-12-06 14:44:32 +0100

    Fixed graphs to reflect changes in train script

--- plot.py
+++ plot.py
@@ -37,15 +37,14 @@ def read_wave(wav_file):
 # python3 plot.py ht1
 # then it searches for Results/ht1-RNN3-ht1/best_val_out.wav
 # investigate actual output from CoreAudioML training.py code
-def analyze_pred_vs_actual(args):
+def analyze_pred_vs_actual(input_wav, output_wav, pred_wav, model_name, show_plots):
     """Generate plots to analyze the predicted signal vs the actual
     signal.
     Inputs:
-        output_wav : The actual signal, by default will use y_test.wav from the test.py output
-        pred_wav : The predicted signal, by default will use y_pred.wav from the test.py output
-        input_wav : The pre effect signal, by default will use x_test.wav from the test.py output
+        input_wav : The pre effect signal
+        output_wav : The actual signal
+        pred_wav : The predicted signal
         model_name : Used to add the model name to the plot .png filename
-        path   :   The save path for generated .png figures
         show_plots : Default is 1 to show plots, 0 to only generate .png files and suppress plots
     1. Plots the two signals
     2. Calculates Error to signal ratio the same way Pedalnet evauluates the model for training
@@ -53,67 +52,48 @@ def analyze_pred_vs_actual(args):
     4. Plots the spectrogram of (pred_signal - actual signal)
          The idea here is to show problem frequencies from the model training
     """
-    try:
-        output_wav = "Data/test/" + args.config_name + args.output_wav
-        pred_wav = "Results/" + args.config_name + "-RNN3-" + args.config_name + "/" + args.pred_wav
-        input_wav = "Data/test/" + args.config_name + args.input_wav
-        model_name = args.config_name
-        show_plots = args.show_plots
-        path = "Results/" + args.config_name + "-RNN3-" + args.config_name
-    except:
-        pred_wav = "Results/" + args["config_name"] + "-RNN3-" + args["config_name"] + "/" + args['pred_Wav']
-        output_wav = "Data/test/" + args["config_name"] + args['output_wav']
-        input_wav = "Data/test/" + args["config_name"] + args['input_wav']
-        model_name = args['config_name']
-        show_plots = args['show_plots']
-        path = "Results/" + args['config_name'] + "-RNN3-" + args['config_name']
+    path = "Results/" + args.config_name + "-RNN3-" + args.config_name
 
     # Read the input wav file
-    signal3, fs3 = read_wave(input_wav)
-    #signal3 = signal3 / 32768.0  ################### quick conversion
+    signal_in, fs_in = read_wave(input_wav)
     # Read the output wav file
-    signal1, fs = read_wave(output_wav)
-    #signal1 = signal1 / 32768.0   ################### quick conversion
+    signal_out, fs_out = read_wave(output_wav)
 
-    Time = np.linspace(0, len(signal1) / fs, num=len(signal1))
+    Time = np.linspace(0, len(signal_out) / fs_out, num=len(signal_out))
     fig, (ax3, ax1, ax2) = plt.subplots(3, sharex=True, figsize=(13, 8))
     fig.suptitle("Predicted vs Actual Signal")
-    ax1.plot(Time, signal1, label=output_wav, color="red")
+    ax1.plot(Time, signal_out, label=output_wav, color="red")
 
     # Read the predicted wav file
-    signal2, fs2 = read_wave(pred_wav)
+    signal_pred, fs_pred = read_wave(pred_wav)
 
-    Time2 = np.linspace(0, len(signal2) / fs2, num=len(signal2))
-    ax1.plot(Time2, signal2, label=pred_wav, color="green")
+    Time2 = np.linspace(0, len(signal_pred) / fs_pred, num=len(signal_pred))
+    ax1.plot(Time2, signal_pred, label=pred_wav, color="green")
     ax1.legend(loc="upper right")
     ax1.set_xlabel("Time (s)")
     ax1.set_ylabel("Amplitude")
     ax1.set_title("Wav File Comparison")
     ax1.grid("on")
 
-    error_list = []
-    for s1, s2 in zip(signal1, signal2):
-        error_list.append(abs(s2 - s1))
-
     # Calculate error to signal ratio with pre-emphasis filter as
     #    used to train the model
-    e2s = error_to_signal(signal1, signal2)
-    e2s_no_filter = error_to_signal(signal1, signal2, use_filter=0)
+    e2s = error_to_signal(signal_out, signal_pred)
+    e2s_no_filter = error_to_signal(signal_out, signal_pred, use_filter=0)
 
     print("Error to signal (with pre-emphasis filter): ", e2s)
     print("Error to signal (no pre-emphasis filter): ", e2s_no_filter)
     fig.suptitle("Predicted vs Actual Signal (error to signal: " + str(round(e2s, 4)) + ")")
     # Plot signal difference
-    signal_diff = signal2 - signal1
-    ax2.plot(Time2, error_list, label="signal diff", color="blue")
+    signal_diff = np.absolute(np.subtract(signal_pred, signal_out))
+    ax2.plot(Time2, signal_diff, label="signal diff", color="blue")
     ax2.set_xlabel("Time (s)")
     ax2.set_ylabel("Amplitude")
     ax2.set_title("abs(pred_signal-actual_signal)")
     ax2.grid("on")
 
     # Plot the original signal
-    Time3 = np.linspace(0, len(signal3) / fs3, num=len(signal3))
-    ax3.plot(Time3, signal3, label=input_wav, color="purple")
+    Time3 = np.linspace(0, len(signal_in) / fs_in, num=len(signal_in))
+    ax3.plot(Time3, signal_in, label=input_wav, color="purple")
     ax3.legend(loc="upper right")
     ax3.set_xlabel("Time (s)")
     ax3.set_ylabel("Amplitude")
@@ -124,30 +104,30 @@ def analyze_pred_vs_actual(args):
     plt.savefig(path+'/'+model_name + "_signal_comparison_e2s_" + str(round(e2s, 4)) + ".png", bbox_inches="tight")
 
     # Create a zoomed in plot of 0.01 seconds centered at the max input signal value
-    sig_temp = signal1.tolist()
+    sig_temp = signal_out.tolist()
     plt.axis(
         [
             Time3[sig_temp.index((max(sig_temp)))] - 0.005,
             Time3[sig_temp.index((max(sig_temp)))] + 0.005,
-            min(signal2),
-            max(signal2),
+            min(signal_pred),
+            max(signal_pred),
         ]
     )
-    plt.savefig(path+'/'+model_name + "_Detail_signal_comparison_e2s_" + str(round(e2s, 4)) + ".png", bbox_inches="tight")
+    plt.savefig(path+'/'+model_name + "_detail_signal_comparison_e2s_" + str(round(e2s, 4)) + ".png", bbox_inches="tight")
 
     # Reset the axis
-    plt.axis([0, Time3[-1], min(signal2), max(signal2)])
+    plt.axis([0, Time3[-1], min(signal_pred), max(signal_pred)])
 
     # Plot spectrogram difference
-    # plt.figure(figsize=(12, 8))
-    # print("Creating spectrogram data..")
-    # frequencies, times, spectrogram = signal.spectrogram(signal_diff, 44100)
-    # plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram))
-    # plt.colorbar()
-    # plt.title("Diff Spectrogram")
-    # plt.ylabel("Frequency [Hz]")
-    # plt.xlabel("Time [sec]")
-    # plt.savefig(path+'/'+model_name + "_diff_spectrogram.png", bbox_inches="tight")
+    plt.figure(figsize=(12, 8))
+    print("Creating spectrogram data..")
+    frequencies, times, spectrogram = signal.spectrogram(signal_diff, fs_in)
+    plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram))
+    plt.colorbar()
+    plt.title("Diff Spectrogram")
+    plt.ylabel("Frequency [Hz]")
+    plt.xlabel("Time [sec]")
+    plt.savefig(path+'/'+model_name + "_diff_spectrogram.png", bbox_inches="tight")
 
     if show_plots == 1:
         plt.show()
@@ -155,13 +135,19 @@ def analyze_pred_vs_actual(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    #parser.add_argument("--path", default=".")
     parser.add_argument("config_name")
-    parser.add_argument("--output_wav", default="-target.wav")
-    parser.add_argument("--pred_wav", default="best_val_out.wav")
-    parser.add_argument("--input_wav", default="-input.wav")
-    parser.add_argument("--model_name", default="plot")
-    parser.add_argument("--path", default="wavs")
     parser.add_argument("--show_plots", default=1)
     args = parser.parse_args()
-    analyze_pred_vs_actual(args)
+    show_plots = args.show_plots
+    # Create graphs on validation data
+    input_wav = "Data/val/" + args.config_name + "-input.wav"
+    output_wav = "Data/val/" + args.config_name + "-target.wav"
+    pred_wav = "Results/" + args.config_name + "-RNN3-" + args.config_name + "/best_val_out.wav"
+    model_name = args.config_name + "_validation"
+    analyze_pred_vs_actual(input_wav, output_wav, pred_wav, model_name, show_plots)
+    # Create graphs on test data
+    input_wav = "Data/test/" + args.config_name + "-input.wav"
+    output_wav = "Data/test/" + args.config_name + "-target.wav"
+    pred_wav = "Results/" + args.config_name + "-RNN3-" + args.config_name + "/test_out_final.wav"
+    model_name = args.config_name + "_test"
+    analyze_pred_vs_actual(input_wav, output_wav, pred_wav, model_name, show_plots)
