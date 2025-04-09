d5b47eb332e6cda9c7bb9e2d56cfb7fb3b9fd3a3
Massimo Pennazio <maxipenna@libero.it>
2025-04-09 17:35:07 +0200

    Update new method to access learning rate

--- dist_model.py
+++ dist_model.py
@@ -275,11 +275,11 @@ if __name__ == "__main__":
             train_track.val_epoch_update(val_loss.item(), val_ep_st_time, time.time())
             writer.add_scalar('TrainingAndValidation/ValidationLoss', train_track['validation_losses'][-1], epoch)
 
-        #print('current learning rate: ' + str(optimiser.param_groups[0]['lr']))
+        #print('current learning rate: ' + str(scheduler.get_last_lr()[0]))
         train_track.train_epoch_update(epoch_loss.item(), ep_st_time, time.time(), init_time, epoch)
         # write loss to the tensorboard (just for recording purposes)
         writer.add_scalar('TrainingAndValidation/TrainingLoss', train_track['training_losses'][-1], epoch)
-        writer.add_scalar('TrainingAndValidation/LearningRate', optimiser.param_groups[0]['lr'], epoch)
+        writer.add_scalar('TrainingAndValidation/LearningRate', scheduler.get_last_lr()[0], epoch)
         network.save_model('model', save_path)
         miscfuncs.json_save(train_track, 'training_stats', save_path)
 
